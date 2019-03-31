321dbba2265b989dd9911da281260f3cb47316ce
Wanjiku <Wanjiku>
2019-03-31 22:57:50 +0300

    linked service to user-component-ts

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user',
@@ -6,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
+ user:any;
+  constructor(private userService: UserService) {
+    this.userService.getProfileInfo().subscribe(user => {
+      this.user = user
+    })
+   }
 
   ngOnInit() {
   }
