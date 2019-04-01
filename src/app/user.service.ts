ebce636dbd8df58c261567847e6844026820f683
Wanjiku <Wanjiku>
2019-04-01 09:45:01 +0300

    added id

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -8,8 +8,8 @@ import { Observable } from 'rxjs';
 })
 export class UserService {
   private username:string;
-  private clientid="";
-  private clientsecret="";
+  private clientid="42ce542e404324d6bdc5";
+  private clientsecret="c21fdf3d1c981dc02f6a8dda9d3fc875a4b5dd5d";
 
   constructor(private http: HttpClient) {
     console.log("service is running")
