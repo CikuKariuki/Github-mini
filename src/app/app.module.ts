8f72e8fe6b25b9f22b3f7c7adab2111e9383b2ba
Wanjiku <Wanjiku>
2019-04-04 14:22:41 +0300

    added a pipe used to count date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 import { UserService } from './user.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { ReposComponent } from './repos/repos.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { DateCountPipe } from './date-count.pipe';
+
+const routes:Routes =[
+  {path:'**',component:NotFoundComponent},
+  {path:'user',component:UserComponent}
+]
 
 @NgModule({
   declarations: [
@@ -16,6 +22,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
     UserComponent,
     ReposComponent,
     NotFoundComponent,
+    DateCountPipe,
   ],
   imports: [
     BrowserModule,
